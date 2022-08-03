6154bbf93a3e78a97811a6384899f775042732d9
bass1st <bass1st@live.com>
2022-08-04 00:02:15 +0200

    Add storefront UI with pager, sort, filter, search

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
@@ -11,14 +7,8 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit {
   title = 'Merch';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe({
-      next: (response: IPagination) => this.products = response.data,
-      error: error => console.log(error)
-    });
-  }
+  ngOnInit(): void { }
 }
