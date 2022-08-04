88c9ddc4482ac9dcbb7da1d21416fd10b6f22184
bass1st <bass1st@live.com>
2022-08-04 14:41:16 +0200

    Add routing and lazy loading

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -6,6 +6,7 @@ import { IBrand } from '../shared/models/brand';
 import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productType';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({ providedIn: 'root' })
 export class ShopService {
@@ -33,7 +34,15 @@ export class ShopService {
     params = params.append('pageSize', shopParams.pageSize.toString());
 
     return this.http.get<IPagination>(this.baseUrl + 'products', { observe: 'response', params })
-      .pipe(map(response => response.body));
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      )
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + 'products/' + id);
   }
 
   getBrands() {
