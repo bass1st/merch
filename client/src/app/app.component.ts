ee416c70ba7647eb1cbac9ec11b1a4d2552075f6
bass1st <bass1st@live.com>
2022-08-08 17:34:32 +0200

    Add client-side login and register with validation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AccountService } from './account/account.service';
 import { BasketService } from './basket/basket.service';
 
 @Component({
@@ -10,14 +10,29 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Merch';
 
-  constructor(private basketService: BasketService) { }
+  constructor(private basketService: BasketService, private accountService: AccountService) { }
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser() {
+    const token = localStorage.getItem('token');
+    this.accountService.loadCurrentUser(token).subscribe(() => {
+      console.log('user loaded');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  loadBasket() {
     const basketId = localStorage.getItem('basket_id');
     if (basketId) {
-      this.basketService.getBasket(basketId).subscribe({
-        next: () => console.log('initialised basket'),
-        error: error => console.log(error)
+      this.basketService.getBasket(basketId).subscribe(() => {
+        console.log('shopping cart initialized');
+      }, error => {
+        console.log(error);
       });
     }
   }
